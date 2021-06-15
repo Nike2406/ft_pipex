1446ee01e49ebc3befd6ba97415cfafe87958d6c
prochell <prochell@student.21-school.ru>
2021-06-15 15:27:49 +0300

    del a_out

--- fork.c
+++ fork.c
@@ -6,20 +6,6 @@
 int	main()
 {
 	int	id = fork();
-	int	n;
-	if (id == 0)
-		n = 1;
-	else
-		n = 6;
-	if (id != 0)
-		wait(&id);
-	int	i;
-	for (i = n; i < n + 5; i++)
-	{
-		printf("%d ", i);
-		fflush(stdout);
-	}
-	if (id != 0)
-		printf("\n");
+
 	return (0);
 }
