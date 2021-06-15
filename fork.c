20db76f0bf3ce59aa26bca9c6368585f37e31633
prochell <prochell@student.21-school.ru>
2021-06-15 15:26:49 +0300

    train wait + fork

--- fork.c
+++ fork.c
@@ -5,7 +5,21 @@
 
 int	main()
 {
-	int id = fork();
-	printf("Hello world from: %d\n", id);
+	int	id = fork();
+	int	n;
+	if (id == 0)
+		n = 1;
+	else
+		n = 6;
+	if (id != 0)
+		wait(&id);
+	int	i;
+	for (i = n; i < n + 5; i++)
+	{
+		printf("%d ", i);
+		fflush(stdout);
+	}
+	if (id != 0)
+		printf("\n");
 	return (0);
 }
