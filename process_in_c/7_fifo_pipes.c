4583f6f469f233a11435c8371d45396feb37f165
prochell <prochell@student.21-school.ru>
2021-06-16 21:41:36 +0300

    add 8

--- process_in_c/7_fifo_pipes.c
+++ process_in_c/7_fifo_pipes.c
@@ -4,3 +4,25 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/errno.h>
+#include <fcntl.h>
+
+int	main()
+{
+	if (mkfifo("myfifo1", 0777) < 0)
+	{
+		if (errno != EEXIST)
+		{
+			printf("Couldn't create fifo file\n");
+			return 1;
+		}
+	}
+	int	fd = open("myfifo1", O_WRONLY);
+	int x = 97;
+	if (write(fd, &x, sizeof(x)) < 0)
+		return 2;
+	close(fd);
+	return (0);
+}
