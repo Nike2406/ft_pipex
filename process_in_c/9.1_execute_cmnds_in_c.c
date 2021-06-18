2ef8bf57b03f8087db8c62e04c24c8068e33a83a
prochell <prochell@student.21-school.ru>
2021-06-18 21:28:36 +0300

    add exec-funcs

--- process_in_c/9.1_execute_cmnds_in_c.c
+++ process_in_c/9.1_execute_cmnds_in_c.c
@@ -1,11 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
-#include <time.h>
-#include <sys/wait.h>
+#include <errno.h>
 
 int	main()
 {
+	/*
+		1 - путь к исполняемому файлу
+		2 - Параметр 1
+		3 - Параметр 2
+		4 - в конце всегда стоит NULL
+	*/
+	execl("/bin/ping", "/bin/ping", "-c", "3", "ya.ru", NULL);
+
 	execlp("ping", "ping", "-c", "3", "ya.ru", NULL);
+
+	char	*arr = {
+		"ping",
+		"ya.ru",
+		NULL
+	};
+	char* env[] = {
+		"TEST=environment variable",
+		NULL
+	};
+	int	err = errno;
+	execvpe("ping", arr, env);
+	/*
+		exec:
+			l - list of parameters (execl);
+			p - path of command (execlp);
+			v - vector, array needs;
+			e - environment;
+	*/
+	printf("Ping finished executing\n");
 	return (0);
 }
