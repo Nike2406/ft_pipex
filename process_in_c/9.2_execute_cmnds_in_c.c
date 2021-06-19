dbd1c3d64cbb568d1895fc1aee7d319f841475dd
prochell <prochell@student.21-school.ru>
2021-06-19 13:16:20 +0300

    finish 9.2

--- process_in_c/9.2_execute_cmnds_in_c.c
+++ process_in_c/9.2_execute_cmnds_in_c.c
@@ -6,6 +6,22 @@
 
 int	main()
 {
-
+	int pid = fork();
+	if (pid < 0)
+		return 1;
+	if (pid == 0)
+	{
+		// Child process
+		execlp("ping", "ping", "-c", "3", "ya.ru", NULL);
+		// exec удаляет все последующие команды в процессе
+		printf("You can't see this in terminal\n");
+	}
+	else
+	{
+		// Parent process
+		wait(NULL);
+		// printf("Some post process\n");
+		printf("Success\n");
+	}
 	return (0);
 }
