bf387d4f404fdb25ad89deec3d801b427489efff
prochell <prochell@student.21-school.ru>
2021-06-19 21:21:19 +0300

    add inf about dup

--- process_in_c/10_redirect_stdout.c
+++ process_in_c/10_redirect_stdout.c
@@ -3,6 +3,17 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
+
+/*
+	child process
+	fd		file
+	0		STDIN
+	1		STDOUT
+	2		STDERR
+	3		pingRsults.txt
+	4		pingRsults.txt
+*/
 
 int	main()
 {
@@ -11,6 +22,18 @@ int	main()
 		return 1;
 	if (pid == 0)
 	{
+		int file = open("pinfResults.txt", O_WRONLY | O_CREAT, 0777);
+		if (file < 0)
+		{
+			printf("Error of OPEN\n");
+			return 3;
+		}
+		printf("The fd to pingResults: %d\n", file);
+		// dup - клонирует процесс и присаивает ему новый fd
+		// dup2 - клонирует процесс и присаивает fd вручную вторым параметром
+		int file2 = dup2(file, STDOUT_FILENO);
+		printf("The duplicated fd: %d\n", file2);
+		close(file);
 		int err = execlp("ping", "ping", "-c", "3", "ya.ru", NULL);
 		if (err < 0)
 		{
