a75808850daac5176738c740a7c9d67d9bb2fc72
prochell <prochell@student.21-school.ru>
2021-06-20 15:32:14 +0300

    add signals

---
+++ process_in_c/11_signals_in_c.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+int	main()
+{
+	int	pid = fork();
+	if(pid < 0)
+		return (1);
+	if (pid == 0)
+	{
+		while (1)
+		{
+			printf("Some text goes here\n");
+			usleep(50000);
+		}
+	}
+	else
+	{
+		sleep(1);
+		kill(pid, SIGKILL);
+		wait(NULL);
+	}
+
+	return (0);
+}
