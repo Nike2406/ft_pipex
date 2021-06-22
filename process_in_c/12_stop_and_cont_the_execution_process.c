ed1cbb94f9b166e5edc1ffc8fae3f41441482580
prochell <prochell@student.21-school.ru>
2021-06-22 21:49:00 +0300

    add stop_cont_signals

---
+++ process_in_c/12_stop_and_cont_the_execution_process.c
@@ -0,0 +1,47 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+int	main()
+{
+	int	pid = fork();
+	if(pid < 0)
+		return (1);
+	if (pid == 0)
+	{
+		while (1)
+		{
+			printf("Some output\n");
+			usleep(50000);
+		}
+	}
+	else
+	{
+		kill(pid, SIGSTOP);
+		int t;
+
+		do {
+			printf("Time in seconds for execution: ");
+			scanf("%d", &t);
+
+			if (t > 0)
+			{
+				kill(pid, SIGCONT);
+				sleep(t);
+				kill(pid, SIGKILL);
+			}
+		} while (t > 0);
+		// kill(pid, SIGSTOP);
+		// sleep(1);
+		// kill(pid, SIGCONT);
+		// sleep(1);
+		// kill(pid, SIGKILL);
+		// wait(NULL);
+	}
+
+	return (0);
+};
