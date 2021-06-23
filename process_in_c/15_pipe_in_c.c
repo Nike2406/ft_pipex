5e9d5a3453773cfdb1e9198388af2e7bf62b1073
prochell <prochell@student.21-school.ru>
2021-06-23 20:58:17 +0300

    fullfill pipe in c

--- process_in_c/15_pipe_in_c.c
+++ process_in_c/15_pipe_in_c.c
@@ -15,9 +15,29 @@ int	main()
 	if (pid1 == 0)
 	{
 		// Child process 1 (ping)
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
 		execlp("ping", "ping", "-c", "5", "ya.ru", NULL);
 	}
+
+	int	pid2 = fork();
+	if (pid2 < 0)
+		return 3;
+	if (pid2 == 0)
+	{
+		// Child process 2 (grep)
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execlp("grep", "grep", "round-trip", NULL);
+	}
+
+	close(fd[0]);
+	close(fd[1]);
+
 	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
 
 	return (0);
 }
