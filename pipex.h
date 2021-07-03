b8d06522cdb8890f99a4d2296a81424261cc5d7d
prochell <prochell@student.21-school.ru>
2021-07-04 00:47:32 +0300

    alexver

--- pipex.h
+++ pipex.h
@@ -13,13 +13,20 @@ typedef struct s_pipex
 {
 	char	**addr;
 	int		cmd_numb;
+	int		pp[100][2];
+	int		pr_pp;
+	int		fd_fl[2];
+	int		argc;
+	char	**argv;
+	int		i;
 }	t_pipex;
 
 char	**path(char **envp);
 void	child_process(char **addr, char **argv, int *fd_pp, int *fd_fl);
 void	parent_process(char **addr, char **argv, int *fd_pp, int *fd_fl);
-void	b_child_process(t_pipex *s_pp, char **argv, int *fd_pp, int *fd_fl);
-void	b_parent_process(t_pipex *s_pp, char **argv, int *fd_pp, int *fd_fl);
-void	ft_err(int	code, t_pipex *s_pp);
+void	b_child_process(t_pipex *s_pp);
+void	b_parent_process(t_pipex *s_pp, int *fd_fl);
+void	ft_err(int	code);
+void	get_open(t_pipex *s_pp);
 
 #endif
--- src/pipex_bonus.c
+++ src/pipex_bonus.c
@@ -22,73 +22,82 @@ char	**path(char **envp)
 	return (addr);
 }
 
-void	get_exec(t_pipex *s_pp, char **argv, int j)
+void	get_exec(t_pipex *s_pp, int j)
 {
 	int		i;
 	char	**cmd;
 
 	i = 0;
-	cmd = ft_split(argv[j], ' ');
+	cmd = ft_split(s_pp->argv[j], ' ');
 	while (s_pp->addr[i])
 	{
 		execve(ft_strjoin(s_pp->addr[i], cmd[0]), cmd, NULL);
 		i++;
 	}
-	i = 0;
-	while (cmd[i])
-		cmd[i++] = NULL;
-	free(cmd);
 }
 
-void	b_child_process(t_pipex *s_pp, char **argv, int *fd_pp, int *fd_fl)
+void	b_child_process(t_pipex *s_pp)
 {
-	close(fd_pp[0]);
-	dup2(fd_fl[0], 0);
-	dup2(fd_pp[1], 1);
-	close(fd_pp[1]);
-	get_exec(s_pp, argv, 2);
+		ft_putstr("Hi!\n");
+	if (s_pp->i == 0)
+	{
+		dup2(s_pp->fd_fl[0], 0);
+		dup2(s_pp->pp[s_pp->i][1], 1);
+		close(s_pp->pp[s_pp->i][0]);
+		close(s_pp->fd_fl[0]);
+	}
+	else if (s_pp->i == s_pp->cmd_numb - 1)
+	{
+		dup2(s_pp->pp[s_pp->i - 1][0], 0);
+		dup2(s_pp->fd_fl[1], 1);
+		close(s_pp->pp[s_pp->i - 1][1]);
+		close(s_pp->fd_fl[1]);
+	}
+	else
+	{
+		dup2(s_pp->pp[s_pp->i - 1][0], 0);
+		dup2(s_pp->pp[s_pp->i][1], 1);
+		close(s_pp->pp[s_pp->i - 1][1]);
+		close(s_pp->pp[s_pp->i][0]);
+	}
+	get_exec(s_pp, s_pp->i);
 }
 
 
-void	b_parent_process(t_pipex *s_pp, char **argv, int *fd_pp, int *fd_fl)
+// void	b_parent_process(t_pipex *s_pp, char **argv, int *fd_fl)
+// {
+
+// 	close(fd_pp[1]);
+// 	dup2(fd_pp[0], 0);
+// 	dup2(fd_fl[1], 1);
+// 	close(fd_pp[0]);
+// 	get_exec(s_pp, argv, 3);
+// 	wait(NULL);
+// }
+
+void get_open(t_pipex *s_pp)
 {
-	fd_fl[1] = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0777);
-	if (fd_fl[1] < 0)
-		ft_err(1, s_pp);
-	close(fd_pp[1]);
-	dup2(fd_pp[0], 0);
-	dup2(fd_fl[1], 1);
-	close(fd_pp[0]);
-	get_exec(s_pp, argv, 3);
-	wait(NULL);
+	s_pp->fd_fl[0] = open(s_pp->argv[1], O_RDONLY, 0777);
+	if (s_pp->fd_fl[0] < 0)
+		ft_err(2);
+	s_pp->fd_fl[1] = open(s_pp->argv[s_pp->argc - 1], O_WRONLY | O_TRUNC | O_CREAT, 0777);
+	if (s_pp->fd_fl[1] < 0)
+		ft_err(1);
 }
 
-void	ft_err(int	code, t_pipex *s_pp)
+void	ft_err(int	code)
 {
 	if (code == 1)
-	{
 		ft_putstr("Please, add more data.\n");
-		free(s_pp);
-		exit(code);
-	}
 	else if (code == 2)
-	{
 		ft_putstr("Error to open input file.\n");
-		free(s_pp);
-		exit(code);
-	}
 	else if (code == 3)
-	{
 		ft_putstr("Error to open the pipe.\n");
-		free(s_pp);
-		exit(code);
-	}
 	else if (code == 4)
-	{
 		ft_putstr("Fork failed.\n");
-		free(s_pp);
-		exit(code);
-	}
+	else if (code == 5)
+		ft_putstr("Malloc failed.\n");
+	exit(code);
 }
 
 void	get_pipe(t_pipex *s_pp)
@@ -96,38 +105,44 @@ void	get_pipe(t_pipex *s_pp)
 	int	i;
 
 	i = 0;
+	// *s_pp->pp = malloc(sizeof(int *) * i);
+	// if (!*s_pp->pp)
+	// 	ft_err(5);
 	while (i < s_pp->cmd_numb - 1)
 	{
-		if (pipe(fd_pp[i]) < 0)
-			ft_err(2, s_pp);
+		// s_pp->pp[i] = malloc(sizeof(int) * 2);
+		if (pipe(s_pp->pp[i]) < 0)
+			ft_err(2);
 		i++;
 	}
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	int		fd_fl[2];
 	int		pid;
-	int		fd_pp[2];
-	t_pipex	*s_pp;
+	t_pipex	s_pp;
 
-	s_pp = (t_pipex *)malloc(sizeof(t_pipex));
+	s_pp.i = -1;
 	if (argc < 5)
-		ft_err(1, s_pp);
-	s_pp->cmd_numb = argc - 3;
-	s_pp->addr = path(envp);
-	fd_fl[0] = open(argv[1], O_RDONLY, 0777);
-	if (fd_fl[0] < 0)
-		ft_err(2, s_pp);
-	if (pipe(fd_pp) < 0)
-		ft_err(3, s_pp);
-	pid = fork();
-	if (pid < 0)
-		ft_err(4, s_pp);
-	if (pid == 0)
-		b_child_process(s_pp, argv, fd_pp, fd_fl);
-	else
-		b_parent_process(s_pp, argv, fd_pp, fd_fl);
-	free(s_pp);
+		ft_err(1);
+	s_pp.argc = argc;
+	s_pp.argv = argv;
+	get_open(&s_pp);
+	s_pp.cmd_numb = argc - 3;
+	s_pp.addr = path(envp);
+	while (++s_pp.i < s_pp.cmd_numb)
+	{
+		get_pipe(&s_pp);
+		pid = fork();
+		if (pid < 0)
+			ft_err(4);
+		if (pid == 0)
+			b_child_process(&s_pp);
+		// else
+		// 	b_parent_process(&s_pp, argv, fd_fl);
+	}
+	s_pp.i = -1;
+	while (++s_pp.i < s_pp.cmd_numb)
+		wait(NULL);
 	return (0);
 }
